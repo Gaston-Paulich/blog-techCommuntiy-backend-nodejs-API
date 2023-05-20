cb1399149940f1b51126917de8d2e5d00023483a
Gaston Paulich <gastopau@quilmes.com.ar>
2023-05-20 03:11:10 -0300

    update 1.3

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // imports
 const app = require('./app');
-const express = require('express');
+
 require('dotenv').config();
 require('./helpers/mongodb.helper');
 
 // constants
 const PORT = process.env.PORT || 5000;
-const app = express();
+
 // start the server
 app.listen(PORT, () => {
     console.log(`server running at port ${PORT}...`);
